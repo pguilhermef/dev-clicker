0fb8af8d96b5cb6325c45112e2e54111f4940524
pguilhermef <pguilhermesanttos@gmail.com>
2026-06-21 17:47:44 -0300

    Initial configurations

---
+++ src/main/java/com/devclicker/dev_clicker/DevClickerApplication.java
@@ -0,0 +1,13 @@
+package com.devclicker.dev_clicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevClickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevClickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devclicker/dev_clicker/DevClickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devclicker.dev_clicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevClickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
