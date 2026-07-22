2c0c2d068443768f2970acd2eafc1255b15d1360
pguilhermef <pguilhermesanttos@gmail.com>
2026-07-22 15:08:29 -0300

    Creates Employee class

--- src/main/java/com/devclicker/dev_clicker/console/model/Employee.java
+++ src/main/java/com/devclicker/dev_clicker/console/model/Employee.java
@@ -1,4 +1,8 @@
 package com.devclicker.dev_clicker.console.model;
 
+import com.devclicker.dev_clicker.console.enums.EmployeeType;
+
 public class Employee {
+    EmployeeType employeeType;
+    Integer codePerMinute;
 }
