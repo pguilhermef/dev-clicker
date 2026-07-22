efbed60bc9d74713e63617e8569af5e1877ceac6
pguilhermef <pguilhermesanttos@gmail.com>
2026-07-22 14:15:20 -0300

    Rename JobTitle to EmployeeType

--- src/main/java/com/devclicker/dev_clicker/console/enums/EmployeeType.java
+++ src/main/java/com/devclicker/dev_clicker/console/enums/EmployeeType.java
@@ -1,24 +1,24 @@
 package com.devclicker.dev_clicker.console.enums;
 
-public enum JobLevel {
+public enum EmployeeType {
     INTERN(50, 5),
     JUNIOR(75, 10),
     MID(125, 15),
     SENIOR(200, 25);
 
-    private final int moneyCost;
-    private final int codePerRound;
+    private final Integer moneyCost;
+    private final Integer codePerRound;
 
-    JobLevel(int moneyCost, int codePerRound) {
+    EmployeeType(Integer moneyCost, Integer codePerRound) {
         this.moneyCost = moneyCost;
         this.codePerRound = codePerRound;
     }
 
-    public int getMoneyCost() {
+    public Integer getMoneyCost() {
         return moneyCost;
     }
 
-    public int getCodePerRound() {
+    public Integer getCodePerRound() {
         return codePerRound;
     }
 }
