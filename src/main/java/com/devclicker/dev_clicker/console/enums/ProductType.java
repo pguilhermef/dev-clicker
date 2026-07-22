7ba1ea335646f219c65b643e359fa0a53556a72a
pguilhermef <pguilhermesanttos@gmail.com>
2026-07-22 14:34:31 -0300

    Creates ProductType Enum

--- src/main/java/com/devclicker/dev_clicker/console/enums/ProductType.java
+++ src/main/java/com/devclicker/dev_clicker/console/enums/ProductType.java
@@ -1,4 +1,23 @@
 package com.devclicker.dev_clicker.console.enums;
 
 public enum ProductType {
+    SITE(100, 1),
+    AUTOMATION(500, 5),
+    APP(2000, 20);
+
+    private final Integer moneyPerRound;
+    private final Integer codeCost;
+
+    ProductType(Integer moneyPerRound, Integer codeCost) {
+        this.moneyPerRound = moneyPerRound;
+        this.codeCost = codeCost;
+    }
+
+    public Integer getMoneyPerRound() {
+        return moneyPerRound;
+    }
+
+    public Integer getCodeCost() {
+        return codeCost;
+    }
 }
