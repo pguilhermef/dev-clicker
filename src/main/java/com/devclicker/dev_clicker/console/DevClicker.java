189481f5ee52a6b1d8960aafeb7294ec9ad16315
pguilhermef <pguilhermesanttos@gmail.com>
2026-07-22 15:58:56 -0300

    Creates constructor, getters and setters of Company

--- src/main/java/com/devclicker/dev_clicker/console/DevClicker.java
+++ src/main/java/com/devclicker/dev_clicker/console/DevClicker.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+package com.devclicker.dev_clicker.console;
+
+import com.devclicker.dev_clicker.console.model.Company;
+import com.devclicker.dev_clicker.console.model.Player;
 
 public class DevClicker {
+    public static void main(String[] args) {
+        Company Oxyan = new Company("Oxyan");
+        Player gui = new Player("Paulo", Oxyan);
+
+        System.out.println(gui.getName());
+        System.out.println(gui.getCompany().getName());
+    }
 }
--- src/main/java/com/devclicker/dev_clicker/console/model/Company.java
+++ src/main/java/com/devclicker/dev_clicker/console/model/Company.java
@@ -1,14 +1,74 @@
 package com.devclicker.dev_clicker.console.model;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Company {
-    String name;
-    Date startDate;
-    Long code;
-    BigDecimal money;
-    List <Employee> employees;
-    List <Product> products;
+    private String name;
+    private LocalDateTime startDate;
+    private Long code;
+    private BigDecimal money;
+    private List <Employee> employees;
+    private List <Product> products;
+
+    public Company(
+     String name
+    ) {
+        this.name = name;
+        this.startDate = LocalDateTime.now();
+        this.code = 0L;
+        this.money = BigDecimal.ZERO;
+        this.employees = new ArrayList<>();
+        this.products = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setCode(Long code){
+        this.code = code;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
