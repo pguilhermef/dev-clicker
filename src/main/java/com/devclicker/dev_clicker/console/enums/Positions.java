d2b4948d6d0f47d456b6c7de4621ccfadf00a086
pguilhermef <pguilhermesanttos@gmail.com>
2026-07-22 14:02:24 -0300

    Create Position Enum

---
+++ src/main/java/com/devclicker/dev_clicker/console/enums/Positions.java
@@ -0,0 +1,24 @@
+package com.devclicker.dev_clicker.console.enums;
+
+public enum Positions {
+    INTERN(50, 5),
+    JUNIOR(75, 10),
+    MID(125, 15),
+    SENIOR(200, 25);
+
+    private final int moneyCost;
+    private final int codePerRound;
+
+    Positions(int moneyCost, int codePerRound) {
+        this.moneyCost = moneyCost;
+        this.codePerRound = codePerRound;
+    }
+
+    public int getMoneyCost() {
+        return moneyCost;
+    }
+
+    public int getCodePerRound() {
+        return codePerRound;
+    }
+}
