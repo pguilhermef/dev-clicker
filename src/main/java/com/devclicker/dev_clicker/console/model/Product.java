77ac6b3b8a9f01138a4cd69c77a7255ae6b5525e
pguilhermef <pguilhermesanttos@gmail.com>
2026-07-22 15:08:55 -0300

    Creates Product class

--- src/main/java/com/devclicker/dev_clicker/console/model/Product.java
+++ src/main/java/com/devclicker/dev_clicker/console/model/Product.java
@@ -1,4 +1,10 @@
 package com.devclicker.dev_clicker.console.model;
 
+import com.devclicker.dev_clicker.console.enums.ProductType;
+
+import java.math.BigDecimal;
+
 public class Product {
+    ProductType productType;
+    BigDecimal incomePerMinute;
 }
