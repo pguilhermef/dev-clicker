1380e72bbc58a9eca84f11baad45b4c3ce5169d7
pguilhermef <pguilhermesanttos@gmail.com>
2026-07-22 15:34:24 -0300

    Creates constructor, getters and setters of Player

--- src/main/java/com/devclicker/dev_clicker/console/model/Player.java
+++ src/main/java/com/devclicker/dev_clicker/console/model/Player.java
@@ -1,6 +1,27 @@
 package com.devclicker.dev_clicker.console.model;
 
 public class Player {
-    String name;
-    Company company;
+    private String name;
+    private Company company;
+
+    public Player(String name, Company company) {
+        this.name = name;
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
